ce7ced4739e5463420b5bb4f6b35796fd603a5a3
Frankles143 <josh-franklin-@hotmail.co.uk>
2018-12-01 14:20:45 +0000

    jank-man might not work

--- Pac-man/Pac-man/Program.cs
+++ Pac-man/Pac-man/Program.cs
@@ -13,7 +13,10 @@ namespace Pac_man
         {
             int score = 0;
 
-            SetUpScreen(ref score);
+            //Maybe use a 2D array later?
+            //string[,] screenArray;
+
+            SetUpScreen( ref score);
 
             Console.ReadLine();
         }
@@ -27,17 +30,22 @@ namespace Pac_man
         {
             //16 tabs long
             //score at the top
-            Console.WriteLine($"                    Score: {score}                          ");
-            Console.WriteLine("                                                             ");
+
+            PacmanOut();
+
+            //Not a good idea
+            Console.WriteLine($"                         Score: {score}                       ");
+            Console.WriteLine("                                                               ");
             Console.WriteLine("_______________________________________________________________");
+            Console.WriteLine("|                              █                              |");
+            Console.WriteLine("| ■■■■■■■■ ■■■■■■              █               _____    __    |");
+            Console.WriteLine("|                              █              |_____|  |__|   |");
             Console.WriteLine("|                                                             |");
             Console.WriteLine("|                                                             |");
             Console.WriteLine("|                                                             |");
-            Console.WriteLine("|                                                             |");
-            Console.WriteLine("|                                                             |");
-            Console.WriteLine("|                                                             |");
-            Console.WriteLine("|                                                             |");
-            Console.WriteLine("|                                                             |");
+            Console.WriteLine("|____                                                     ____|");
+            Console.WriteLine("                                                               ");
+            Console.WriteLine(" ____                                                     ____ ");
             Console.WriteLine("|                                                             |");
             Console.WriteLine("|                                                             |");
             Console.WriteLine("|                                                             |");
@@ -45,6 +53,40 @@ namespace Pac_man
             Console.WriteLine("|                                                             |");
             Console.WriteLine("|                                                             |");
             Console.WriteLine("|_____________________________________________________________|");
+
+
+        }
+
+        static void PacmanOut()
+        {
+
+            Console.WriteLine(@"                                                              _____           ");
+            Console.WriteLine(@"                                                          ,-~       ~-.       ");
+            Console.WriteLine(@"                                                        ,^ ___     ___ ^.     ");
+            Console.WriteLine(@"                                                       / .^   ^. .^   ^. \    ");
+            Console.WriteLine(@"                                                      Y  l    O! l    O!  Y   ");
+            Console.WriteLine(@" _ __   __ _  ___ _ __ ___   __ _ _ __                l_ `.___.' `.___.' _[   ");
+            Console.WriteLine(@"| '_ \ / _` |/ __| '_ ` _ \ / _` | '_ \               l^~ ------------ -~^I   ");
+            Console.WriteLine(@"| |_) | (_| | (__| | | | | | (_| | | | |              !\,               ,/!   ");
+            Console.WriteLine(@"| .__/ \__,_|\___|_| |_| |_|\__,_|_| |_|               \ ~-.,_______,.-~ /    ");
+            Console.WriteLine(@"| |                                                     ^.             .^     ");
+            Console.WriteLine(@"|_|                                                        -.._____., -       ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
